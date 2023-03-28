6d4c7b473cd93332aed3fe2eb1d74cf402a71bfe
hoangminh24 <127291509+hoangminh24@users.noreply.github.com>
2023-03-28 09:49:40 +0700

    Add files via upload

---
+++ Migrations/202303270741229_AddFollowingsTable.cs
@@ -0,0 +1,39 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFollowingsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Followings",
+                c => new
+                    {
+                        FollowerId = c.String(nullable: false, maxLength: 128),
+                        FolloweeId = c.String(nullable: false, maxLength: 128),
+                        ApplicationUser_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.FollowerId, t.FolloweeId })
+                .ForeignKey("dbo.AspNetUsers", t => t.FolloweeId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.FollowerId)
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUser_Id)
+                .Index(t => t.FollowerId)
+                .Index(t => t.FolloweeId)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Followings", "ApplicationUser_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Followings", "FollowerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Followings", "FolloweeId", "dbo.AspNetUsers");
+            DropIndex("dbo.Followings", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.Followings", new[] { "FolloweeId" });
+            DropIndex("dbo.Followings", new[] { "FollowerId" });
+            DropTable("dbo.Followings");
+        }
+    }
+}
